bb91131c59d2db3588b8b850616fe267c190b71b
tmastrom <thomas.mastromonaco@gmail.com>
2022-08-30 16:53:57 -0700

    chore: fix user table naming and update user table verification params

--- schema/verify/tables/eed_user.sql
+++ schema/verify/tables/eed_user.sql
@@ -2,16 +2,24 @@
 
 begin;
 
-select pg_catalog.has_table_privilege('eed.user', 'select');
+select pg_catalog.has_table_privilege('eed.eed_user', 'select');
 
 -- eed_internal Grants
-select eed_private.verify_grant('select', 'user', 'eed_internal');
-select eed_private.verify_grant('insert', 'user', 'eed_internal');
-select eed_private.verify_grant('update', 'user', 'eed_internal');
+select eed_private.verify_grant('select', 'eed_user', 'eed_internal');
+select eed_private.verify_grant('insert', 'eed_user', 'eed_internal');
+select eed_private.verify_grant('update', 'eed_user', 'eed_internal',
+    ARRAY['given_name', 'family_name', 'email_address', 'created_at', 'updated_at', 'archived_at']);
+
+-- eed_external Grants
+select eed_private.verify_grant('select', 'eed_user', 'eed_external');
+select eed_private.verify_grant('insert', 'eed_user', 'eed_external');
+select eed_private.verify_grant('update', 'eed_user', 'eed_external',
+    ARRAY['given_name', 'family_name', 'email_address', 'created_at', 'updated_at', 'archived_at']);
 
 -- eed_admin Grants
-select eed_private.verify_grant('select', 'user', 'eed_admin');
-select eed_private.verify_grant('insert', 'user', 'eed_admin');
-select eed_private.verify_grant('update', 'user', 'eed_admin');
+select eed_private.verify_grant('select', 'eed_user', 'eed_admin');
+select eed_private.verify_grant('insert', 'eed_user', 'eed_admin');
+select eed_private.verify_grant('update', 'eed_user', 'eed_admin',
+    ARRAY['given_name', 'family_name', 'email_address', 'created_at', 'updated_at', 'archived_at']);
 
 rollback;
