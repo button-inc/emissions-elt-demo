d36dc408d6177c576278b45f68c2ac5fd1ec4d12
Mike Vesprini <11034827+mikevespi@users.noreply.github.com>
2022-09-14 11:33:01 -0700

    feat: terraform setup for provisioning cloudsql with pg db (#26)
    * feat: terraform setup for provisioning cloudsql with pg db
    
    * chore: terraform fmt
    
    * chore: change db name to var in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.35.0"
+      version = "3.60"
     }
   }
 }
@@ -13,4 +13,71 @@ provider "google" {
   project = var.project
   region  = var.region
   zone    = var.zone
+}
+
+provider "google-beta" {
+  project     = var.project
+  region      = var.region
+  credentials = file("gcp_sa_secret.json")
+}
+
+# create db instance
+resource "google_sql_database_instance" "postgresql" {
+  name                = var.db_name
+  project             = var.project
+  region              = var.region
+  database_version    = var.db_version
+  deletion_protection = false
+
+  settings {
+    tier              = var.db_tier
+    activation_policy = var.db_activation_policy
+    disk_autoresize   = var.db_disk_autoresize
+    disk_size         = var.db_disk_size
+    disk_type         = var.db_disk_type
+    pricing_plan      = var.db_pricing_plan
+
+    location_preference {
+      zone = var.zone
+    }
+
+    maintenance_window {
+      day  = "7" # sunday
+      hour = "3" # 3am
+    }
+
+    backup_configuration {
+      enabled    = true
+      start_time = "00:00"
+    }
+
+    ip_configuration {
+      ipv4_enabled = "true"
+      authorized_networks {
+        value = var.db_instance_access_cidr
+      }
+    }
+  }
+}
+
+# create db
+resource "google_sql_database" "postgresql_db" {
+  name      = var.db_name
+  project   = var.project
+  instance  = google_sql_database_instance.postgresql.name
+  charset   = var.db_charset
+  collation = var.db_collation
+}
+
+# create user
+resource "random_id" "user_password" {
+  byte_length = 8
+}
+resource "google_sql_user" "postgresql_user" {
+  name     = var.db_user_name
+  project  = var.project
+  instance = google_sql_database_instance.postgresql.name
+  host     = var.db_user_host
+  password = (var.db_user_password == "" ?
+  random_id.user_password.hex : var.db_user_password)
 }
