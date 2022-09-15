017c807b9b72bc32e774c8b89178675f36719d07
Mike Vesprini <mike@button.is>
2022-09-15 16:26:18 -0700

    feat/terraform code to provision GKE instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,3 +90,38 @@ resource "google_artifact_registry_repository" "eed-artifacts" {
   description   = "eed docker repository"
   format        = "DOCKER"
 }
+
+# GKE
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "eed-gke"
+  location = var.region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "eed-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
