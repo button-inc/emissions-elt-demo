a0077a1fbd33dd4eec9337a88ba3df71b80cd5fd
Mike Vesprini <mike@button.is>
2022-09-16 14:17:58 -0700

    docs: explain the change from gke default config

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,6 +106,13 @@ resource "google_container_cluster" "primary" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
+
+  # This is set to maintain idempotency. Without this empty block declared,
+  # deafult settings are applied, and on every `terraform apply` it forces
+  # a destroy and rebuild
+  # From the docs: "It's recommended that you create a VPC-native cluster,
+  # done by specifying the ip_allocation_policy block or using secondary ranges on existing subnet."
+  # https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/using_gke_with_terraform
   ip_allocation_policy {}
 }
 
