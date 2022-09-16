1837a9e9cc695bad32d5a8ecb1186dbab6b3f391
Mike Vesprini <mike@button.is>
2022-09-16 14:01:56 -0700

    feat: add gke resource to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,6 +106,7 @@ resource "google_container_cluster" "primary" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
+  ip_allocation_policy {}
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
@@ -113,15 +114,4 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
 }
