16c84ae306bb5e8e69cb7169ef1b271a2137719d
Mike Vesprini <11034827+mikevespi@users.noreply.github.com>
2022-09-16 14:31:42 -0700

    feat: provision GKE instance with terraform (#43)
    * feat/terraform code to provision GKE instance
    
    * feat: add gke resource to terraform
    
    * docs: explain the change from gke default config

--- terraform/postgresql-instance-variables.tf
+++ terraform/postgresql-instance-variables.tf
@@ -51,7 +51,7 @@ variable "db_user_name" {
 }
 variable "db_user_host" {
   description = "The host for the default user"
-  default     = "%"
+  default     = ""
 }
 variable "db_user_password" {
   description = "The password for the default user. If not set, a random one will be generated and available in the generated_user_password output variable."
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,8 @@ variable "artifact_respository_name" {
   default     = "eed-docker-artifact-repository"
   description = "name of the Docker artifact repository for the project"
 }
+
+variable "gcp_service_account_email" {
+  default     = "eed-terraform-svc-acct@emissions-elt-demo.iam.gserviceaccount.com"
+  description = "email address of the sercice account used to provision resources"
+}
