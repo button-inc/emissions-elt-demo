04f6831a6655b30a25e039aa27b39f4f5f54310f
Mike Vesprini <mike@button.is>
2022-09-25 20:10:02 -0700

    chore: save work in progress on cloud run tf

---
+++ terraform/modules/cloud_compose/variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+  default     = "emissions-elt-demo"
+  description = "project id from GCP"
+}
+variable "project_number" {
+  default     = "emissions-elt-demo"
+  description = "project number from GCP"
+}
+variable "region" {
+  default     = "us-west1"
+  description = "GCP region"
+}
