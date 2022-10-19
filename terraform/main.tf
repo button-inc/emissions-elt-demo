36f49c2c06e8531fb8b04a502e5a1574f74633e5
Dylan Leard <dylan@button.is>
2022-10-19 11:58:42 -0700

    feat: composer pulls secrets from postgres module

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,6 +38,10 @@ module "cloud_composer" {
   project        = var.project
   project_number = var.project_number
   region         = var.region
+  ELT_DB_HOST    = module.postgres.db_instance_address
+  ELT_DB_USER    = module.postgres.db_instance_username
+  ELT_DB_PASS    = module.postgres.db_instance_generated_user_password
+  depends_on = [module.postgres]
 }
 
 module "tfstate_bucket" {
--- terraform/modules/cloud_composer/variables.tf
+++ terraform/modules/cloud_composer/variables.tf
@@ -9,3 +9,18 @@ variable "region" {
   default     = "us-west4"
   description = "GCP region"
 }
+
+variable "ELT_DB_HOST" {
+  default     = ""
+  description = "Host IP for the ELT database"
+}
+
+variable "ELT_DB_USER" {
+  default     = ""
+  description = "Username for the ELT database"
+}
+
+variable "ELT_DB_PASS" {
+  default     = ""
+  description = "Password for the ELT database"
+}
