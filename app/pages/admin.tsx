e363112cb3750d10d88df11b07d97f507008671c
Josh Gamache <joshua@button.is>
2022-11-19 01:09:38 -0700

    chore: add basic layout to approximate mocks

--- app/pages/admin.tsx
+++ app/pages/admin.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@button-inc/button-theme";
+import { Button, Grid } from "@button-inc/button-theme";
 import Link from "next/link";
 
 export default function Admin() {
@@ -10,14 +10,24 @@ export default function Admin() {
   ];
   return (
     <>
-      {/* TODO(JG): Wrap in default layout */}
-      {adminOptions.map((option) => (
-        <Link href={option.link} key={option.title}>
-          <Button size="large" variant="secondary">
-            {option.title}
-          </Button>
-        </Link>
-      ))}
+      {/* TODO(JG): Wrap in default layout, header, footer */}
+      <Grid style={{ padding: "2rem" }}>
+        <Grid.Row justify="space-around" align="center">
+          {adminOptions.map((option) => (
+            <Grid.Col key={option.title} span="4">
+              <Link href={option.link}>
+                <Button
+                  size="large"
+                  variant="secondary"
+                  style={{ width: "100%" }}
+                >
+                  {option.title}
+                </Button>
+              </Link>
+            </Grid.Col>
+          ))}
+        </Grid.Row>
+      </Grid>
     </>
   );
 }
