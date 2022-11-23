29c6adb85fda3be3f7ad30335c092b0db5fc6f19
Josh Gamache <joshua@button.is>
2022-11-23 13:45:21 -0700

    Admin page (#100)
    * chore: add button-theme component library
    
    * feat: add admin page
    
    * chore: update button-theme lib incorporating bugfix
    
    * chore: add basic layout to approximate mocks
    
    * test: test admin page in temp default layout

---
+++ app/pages/admin.tsx
@@ -0,0 +1,29 @@
+import { Button, Grid } from '@button-inc/button-theme';
+import Link from "next/link";
+
+export default function Admin() {
+  // TODO(JG): Add links to actual pages once created
+  const adminOptions = [
+    { title: "Add a New Dataset", link: "#" },
+    { title: "View Imported Datasets", link: "#" },
+    { title: "Data Insights (Metabase)", link: "#" },
+  ];
+  return (
+    <>
+      {/* TODO(JG): Wrap in default layout, header, footer */}
+      <Grid style={{ padding: '2rem' }}>
+        <Grid.Row justify="space-around" align="center">
+        {adminOptions.map((option) => (
+          <Grid.Col key={option.title} span="4">
+            <Link href={option.link}>
+              <Button size="large" variant="secondary" style={{ width: '100%' }}>
+                {option.title}
+              </Button>
+            </Link>
+          </Grid.Col>
+        ))}
+        </Grid.Row>
+      </Grid>
+    </>
+  );
+}
