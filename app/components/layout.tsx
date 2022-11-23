f988097d50b5ec5734e6ec89275df37ab44afd27
Josh Gamache <joshua@button.is>
2022-11-23 14:01:37 -0700

    test: test admin page in temp default layout

---
+++ app/components/layout.tsx
@@ -0,0 +1,11 @@
+import { Footer, Header } from "@button-inc/button-theme";
+
+export default function Layout({ children }) {
+  return (
+    <div className="page-container">
+      <Header />
+      <main className="content--vertical-center">{children}</main>
+      <Footer className="footer" />
+    </div>
+  );
+}
