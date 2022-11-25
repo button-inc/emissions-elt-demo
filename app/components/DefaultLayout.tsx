2cd55e2fd6e84433baef2a0a3d8dc3a24227faed
Ballard Robinett <ballardrobinett@gmail.com>
2022-11-25 12:49:26 -0500

    chore: added default layout with header (#101)
    * chore: added default layout with header
    
    * chore: removed unused import
    
    * chore: fixed style for prettier
    
    * chore: fixed style for prettier again
    
    * chore: fixed style for prettier again again
    
    * chore: fixed style for prettier again again again

--- app/components/DefaultLayout.tsx
+++ app/components/DefaultLayout.tsx
@@ -1,11 +1,10 @@
-import { Footer, Header } from "@button-inc/button-theme";
+import { Header } from "@button-inc/button-theme";
 
-export default function Layout({ children }) {
+export default function DefaultLayout({ children }) {
   return (
     <div className="page-container">
       <Header />
       <main className="content--vertical-center">{children}</main>
-      <Footer className="footer" />
     </div>
   );
 }
