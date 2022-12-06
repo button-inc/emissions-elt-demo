04852ae06a5e692011ffef13492f769c2da51bd7
Josh Gamache <joshua@button.is>
2022-12-06 11:11:04 -0700

    fix: use single default layout in app (#117)
    * fix(ui): utilize default layout for whole app and correct admin page
    
    * fix: use default layout for pages

--- app/pages/admin.tsx
+++ app/pages/admin.tsx
@@ -1,6 +1,5 @@
 import { Button, Grid } from "@button-inc/button-theme";
 import Link from "next/link";
-import DefaultLayout from "components/DefaultLayout";
 
 export default function Admin() {
   const adminOptions = [
@@ -11,25 +10,23 @@ export default function Admin() {
   ];
   return (
     <>
-      <DefaultLayout>
-        <Grid style={{ padding: "2rem" }}>
-          <Grid.Row justify="space-around" align="center">
-            {adminOptions.map((option) => (
-              <Grid.Col key={option.title} span="30">
-                <Link href={option.link}>
-                  <Button
-                    size="large"
-                    variant="secondary"
-                    style={{ width: "100%" }}
-                  >
-                    {option.title}
-                  </Button>
-                </Link>
-              </Grid.Col>
-            ))}
-          </Grid.Row>
-        </Grid>
-      </DefaultLayout>
+      <Grid style={{ padding: "2rem" }}>
+        <Grid.Row justify="space-around" align="center">
+          {adminOptions.map((option) => (
+            <Grid.Col key={option.title} span="30">
+              <Link href={option.link}>
+                <Button
+                  size="large"
+                  variant="secondary"
+                  style={{ width: "100%" }}
+                >
+                  {option.title}
+                </Button>
+              </Link>
+            </Grid.Col>
+          ))}
+        </Grid.Row>
+      </Grid>
     </>
   );
 }
