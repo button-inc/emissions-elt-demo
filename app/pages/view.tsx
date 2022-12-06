dae127dcbc1941d22ed48d09de79560f1d403344
Josh Gamache <joshua@button.is>
2022-12-06 08:00:45 -0700

    fix: use default layout for pages

--- app/pages/view.tsx
+++ app/pages/view.tsx
@@ -1,6 +1,5 @@
 import { Button, Grid } from "@button-inc/button-theme";
 import Link from "next/link";
-import DefaultLayout from "components/DefaultLayout";
 
 export default function View() {
   const viewOptions = [
@@ -10,25 +9,23 @@ export default function View() {
   ];
   return (
     <>
-      <DefaultLayout>
-        <Grid style={{ padding: "2rem" }}>
-          <Grid.Row justify="space-around" align="center">
-            {viewOptions.map((option) => (
-              <Grid.Col key={option.title} span="30">
-                <Link href={option.link}>
-                  <Button
-                    size="large"
-                    variant="secondary"
-                    style={{ width: "100%" }}
-                  >
-                    {option.title}
-                  </Button>
-                </Link>
-              </Grid.Col>
-            ))}
-          </Grid.Row>
-        </Grid>
-      </DefaultLayout>
+      <Grid style={{ padding: "2rem" }}>
+        <Grid.Row justify="space-around" align="center">
+          {viewOptions.map((option) => (
+            <Grid.Col key={option.title} span="30">
+              <Link href={option.link}>
+                <Button
+                  size="large"
+                  variant="secondary"
+                  style={{ width: "100%" }}
+                >
+                  {option.title}
+                </Button>
+              </Link>
+            </Grid.Col>
+          ))}
+        </Grid.Row>
+      </Grid>
     </>
   );
 }
