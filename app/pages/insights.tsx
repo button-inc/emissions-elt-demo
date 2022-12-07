f69dd27b5345dfe1387d5ed59501813b0b068d28
Josh Gamache <joshua@button.is>
2022-12-07 14:44:30 -0700

    fix: use single default layout in app
    * fix(ui): utilize default layout for whole app and correct admin page
    
    * fix: use default layout for pages

--- app/pages/insights.tsx
+++ app/pages/insights.tsx
@@ -1,6 +1,5 @@
 import { Button, Grid } from "@button-inc/button-theme";
 import Link from "next/link";
-import DefaultLayout from "components/DefaultLayout";
 
 export default function Insights() {
   const insightsOptions = [
@@ -9,25 +8,23 @@ export default function Insights() {
   ];
   return (
     <>
-      <DefaultLayout>
-        <Grid style={{ padding: "2rem" }}>
-          <Grid.Row justify="space-around" align="center">
-            {insightsOptions.map((option) => (
-              <Grid.Col key={option.title} span="30">
-                <Link href={option.link}>
-                  <Button
-                    size="large"
-                    variant="secondary"
-                    style={{ width: "100%" }}
-                  >
-                    {option.title}
-                  </Button>
-                </Link>
-              </Grid.Col>
-            ))}
-          </Grid.Row>
-        </Grid>
-      </DefaultLayout>
+      <Grid style={{ padding: "2rem" }}>
+        <Grid.Row justify="space-around" align="center">
+          {insightsOptions.map((option) => (
+            <Grid.Col key={option.title} span="30">
+              <Link href={option.link}>
+                <Button
+                  size="large"
+                  variant="secondary"
+                  style={{ width: "100%" }}
+                >
+                  {option.title}
+                </Button>
+              </Link>
+            </Grid.Col>
+          ))}
+        </Grid.Row>
+      </Grid>
     </>
   );
 }
--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,30 +1,27 @@
 import { Button, Grid } from "@button-inc/button-theme";
 import Link from "next/link";
-import DefaultLayout from "components/DefaultLayout";
 
 export default function Login() {
   const loginOptions = [{ title: "Log in", link: "home" }];
   return (
     <>
-      <DefaultLayout>
-        <Grid style={{ padding: "2rem" }}>
-          <Grid.Row justify="space-around" align="center">
-            {loginOptions.map((option) => (
-              <Grid.Col key={option.title} span="30">
-                <Link href={option.link}>
-                  <Button
-                    size="large"
-                    variant="secondary"
-                    style={{ width: "100%" }}
-                  >
-                    {option.title}
-                  </Button>
-                </Link>
-              </Grid.Col>
-            ))}
-          </Grid.Row>
-        </Grid>
-      </DefaultLayout>
+      <Grid style={{ padding: "2rem" }}>
+        <Grid.Row justify="space-around" align="center">
+          {loginOptions.map((option) => (
+            <Grid.Col key={option.title} span="30">
+              <Link href={option.link}>
+                <Button
+                  size="large"
+                  variant="secondary"
+                  style={{ width: "100%" }}
+                >
+                  {option.title}
+                </Button>
+              </Link>
+            </Grid.Col>
+          ))}
+        </Grid.Row>
+      </Grid>
     </>
   );
 }
