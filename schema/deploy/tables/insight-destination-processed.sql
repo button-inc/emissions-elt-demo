f90cbce80bcabdcd4abb9644d642698fa9c3176c
Josh Gamache <joshua@button.is>
2022-12-19 16:54:24 -0700

    feat: add db plan for insights data tables

---
+++ schema/deploy/tables/insight-destination-processed.sql
@@ -0,0 +1,28 @@
+-- Deploy eed:tables/insight-destination-processed to pg
+
+BEGIN;
+
+create table eed.study_area(
+  study_area_id SERIAL PRIMARY KEY,
+  area_name TEXT not null,
+  updated_at TIMESTAMP not null DEFAULT NOW()
+);
+
+-- Primary key is a pair of (origin_area_id, destination_area_id)
+create table eed.insights_voyage(
+  origin_area_id INTEGER REFERENCES eed.study_area(study_area_id),
+  destination_area_id INTEGER REFERENCES eed.study_area(study_area_id),
+  voyage_count INTEGER not null CHECK (voyage_count >= 0),
+  updated_at TIMESTAMP not null DEFAULT NOW(),
+  PRIMARY KEY (origin_area_id, destination_area_id)
+);
+
+create table eed.area_distance_map(
+  origin_area_id INTEGER REFERENCES eed.study_area(study_area_id),
+  destination_area_id INTEGER REFERENCES eed.study_area(study_area_id),
+  distance INTEGER not null CHECK (distance >= 0),
+  updated_at TIMESTAMP not null DEFAULT NOW(),
+  PRIMARY KEY (origin_area_id, destination_area_id)
+);
+
+COMMIT;
