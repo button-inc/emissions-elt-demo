94ba7f3ec4ab40cad57748d32b768ac5ff90312b
Ballard Robinett <ballard@button.is>
2023-01-06 13:20:22 -0500

    chore: modified roles and users table (#131)

--- schema/deploy/add_test_user.sql
+++ schema/deploy/add_test_user.sql
@@ -5,8 +5,7 @@ BEGIN;
 
 CREATE USER eed_test_user WITH LOGIN PASSWORD 'secret';
 
---GRANT eed_internal to eed_test_user;
-SELECT eed_private.insert_user('eed_test_user', 'secret', 'eed_test_user@button.is');
+GRANT eed_internal to eed_test_user;
 
 
 COMMIT;
--- schema/deploy/insert_user.sql
+++ schema/deploy/insert_user.sql
@@ -5,14 +5,12 @@ BEGIN;
 
 SET client_min_messages = 'warning';
 
-CREATE EXTENSION "uuid-ossp";
-
 CREATE OR REPLACE FUNCTION eed_private.insert_user(
     username varchar(1000),
-    password varchar(1000),
-    email varchar(1000)
+    email varchar(1000),
+    user_role varchar(1000)
 ) RETURNS VOID LANGUAGE SQL SECURITY DEFINER AS $$
-    INSERT INTO eed.users (uuid, username, password, email) VALUES((SELECT uuid_generate_v4()), $1, md5($2), $3);
+    INSERT INTO eed.users (username, email, user_role) VALUES($1, $2, $3);
 $$;
 
 COMMIT;
