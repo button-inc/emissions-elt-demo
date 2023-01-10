eb2dd960d1d6e06f69e7e768305f088dcbe18533
Josh Gamache <joshua@button.is>
2023-01-10 09:21:45 -0700

    feat: Insights ingestion tables (#132)
    * feat: add db plan for `import record` table
    
    fix: add user and track format to validation
    
    * feat: add db plan for insights data tables

---
+++ schema/verify/tables/insight-destination-processed.sql
@@ -0,0 +1,17 @@
+-- Verify eed:tables/insight-destination-processed on pg
+
+BEGIN;
+
+select study_area_id, area_name, updated_at
+  from eed.study_area
+  where false;
+
+select origin_area_id, destination_area_id, voyage_count, start_time, updated_at
+  from eed.insights_voyage
+  where false;
+
+select origin_area_id, destination_area_id, distance, updated_at
+  from eed.area_distance_map
+  where false;
+
+ROLLBACK;
