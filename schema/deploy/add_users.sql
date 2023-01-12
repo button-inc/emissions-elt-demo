5bda7a4ea2ffba5c35e28761a943bec052fa8ef3
Ballard <ballard@button.is>
2023-01-12 15:09:50 -0500

    chore: fixed typo

--- schema/deploy/add_users.sql
+++ schema/deploy/add_users.sql
@@ -5,7 +5,7 @@ BEGIN;
 SELECT eed_private.insert_user('shon@button.is', 'analyst');
 SELECT eed_private.insert_user('ballard@button.is', 'analyst');
 SELECT eed_private.insert_user('joshua@button.is', 'analyst');
-SELECT eed_private.insert_user('shon@hogan@gmail.com', 'manager');
+SELECT eed_private.insert_user('shon.hogan@gmail.com', 'manager');
 SELECT eed_private.insert_user('ballardrobinett@gmail.com', 'dropper');
 
 COMMIT;
--- schema/revert/add_users.sql
+++ schema/revert/add_users.sql
@@ -5,7 +5,7 @@ BEGIN;
 DELETE FROM eed.permissions WHERE email = 'shon@button.is';
 DELETE FROM eed.permissions WHERE email = 'ballard@button.is';
 DELETE FROM eed.permissions WHERE email = 'joshua@button.is';
-DELETE FROM eed.permissions WHERE email = 'shon@hogan@gmail.com';
+DELETE FROM eed.permissions WHERE email = 'shon.hogan@gmail.com';
 DELETE FROM eed.permissions WHERE email = 'ballardrobinett@gmail.com';
 
 COMMIT;
