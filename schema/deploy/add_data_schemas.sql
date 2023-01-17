88f02437fdb788e351a1b3f628213ec0d6fcb7cd
Ballard Robinett <ballard@button.is>
2023-01-17 16:27:08 -0500

    chore: added data schemas and role permissions (#147)
    * chore: added data schemas and role permissions
    
    * chore: fixed typo

---
+++ schema/deploy/add_data_schemas.sql
@@ -0,0 +1,24 @@
+-- Deploy eed:add_data_schemas to pg
+
+BEGIN;
+
+create schema data_clean_room;
+grant all on schema data_clean_room to eed_internal, eed_external, eed_admin;
+grant all on schema data_clean_room to analyst;
+grant all on schema data_clean_room to dropper;
+comment on schema data_clean_room is 'The schema for collected data to be aggregated and anonymized.';
+
+create schema data_science_workspace;
+grant all on schema data_science_workspace to eed_internal, eed_external, eed_admin;
+grant all on schema data_science_workspace to analyst;
+grant all on schema data_science_workspace to manager;
+comment on schema data_science_workspace is 'The schema for anonymized data to be analyzed.';
+
+create schema published_vault;
+grant all on schema published_vault to eed_internal, eed_external, eed_admin;
+grant all on schema published_vault to analyst;
+grant all on schema published_vault to manager;
+grant all on schema published_vault to dropper;
+comment on schema published_vault is 'The schema for published data with public access.';
+
+COMMIT;
