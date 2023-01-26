936ca285ad22943a567f51e2dfba1268a28ab773
Ballard Robinett <ballard@button.is>
2023-01-26 13:19:39 -0500

    chore: adding gabriel to permissions table (#180)

--- schema/verify/add_users.sql
+++ schema/verify/add_users.sql
@@ -7,5 +7,6 @@ SELECT 1 FROM eed.permissions WHERE email = 'ballard@button.is';
 SELECT 1 FROM eed.permissions WHERE email = 'joshua@button.is';
 SELECT 1 FROM eed.permissions WHERE email = 'shon.hogan@gmail.com';
 SELECT 1 FROM eed.permissions WHERE email = 'ballardrobinett@gmail.com';
+SELECT 1 FROM eed.permissions WHERE email = 'gabriel@button.iss';
 
 ROLLBACK;
