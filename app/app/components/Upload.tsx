c965e9df2b09bd8b637fe7864f514e9b126e0802
Ballard Robinett <ballard@button.is>
2023-01-30 15:51:27 -0500

    chore: added upload page with functionality (#192)
    * chore: added upload page with functionality
    
    * chore: removed token string

---
+++ app/app/components/Upload.tsx
@@ -0,0 +1,126 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+const URL_ENDPOINT =
+  "https://storage.googleapis.com/upload/storage/v1/b/eed_upload_file_storage/o?uploadType=multipart";
+function Form() {
+  // 👇 These are wrapped around the file data to form the request body.
+  const bodyStart =
+    "--my-boundary\n" +
+    "Content-Type: application/json; charset=UTF-8\n\n" +
+    '{"name": "myObject.csv"}\n\n' +
+    "--my-boundary\n\n";
+  const bodyEnd = "\n\n--my-boundary--";
+
+  function combineBody(middle): any {
+    return bodyStart + middle + bodyEnd;
+  }
+  // 👇 This token expires and must be refreshed through the Google OAuth Playground
+  const authToken = "";
+
+  let binaryData;
+
+  const [formStatus, setFormStatus] = useState(false);
+  const [query, setQuery] = useState({
+    dataConnector: "",
+    file: "",
+  });
+  const handleFileChange = () => (e) => {
+    setQuery((prevState) => ({
+      ...prevState,
+      files: e.target.files[0],
+    }));
+  };
+  const handleChange = () => (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuery((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let file = document.getElementById("file").files[0];
+    let reader = new FileReader();
+    reader.onload = function () {
+      binaryData = reader.result;
+      let stringData = new TextDecoder().decode(binaryData);
+
+      axios
+        .post(URL_ENDPOINT, combineBody(stringData), {
+          headers: {
+            Authorization: "Bearer " + authToken,
+            "Content-Type": "multipart/related; boundary=my-boundary",
+          },
+        })
+        .then(function (response) {
+          setFormStatus(true);
+          setQuery({
+            dataConnector: "",
+            file: "",
+          });
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+    reader.readAsArrayBuffer(file);
+
+    const formData = new FormData();
+    Object.entries(query).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+  };
+  return (
+    <div className="container-md">
+      <h2>Choose a data connector</h2>
+      <form
+        acceptCharset="UTF-8"
+        method="POST"
+        encType="multipart/form-data"
+        id="ajaxForm"
+        onSubmit={handleSubmit}
+      >
+        <div className="form-group">
+          <label htmlFor="exampleFormControlSelect1">
+            Select a data connector:{" "}
+          </label>
+          <select
+            className="form-control"
+            id="exampleFormControlSelect1"
+            required
+            name="dataConnector"
+            value={query.dataConnector}
+            onChange={handleChange()}
+          >
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+          </select>
+        </div>
+        <hr />
+        <div className="form-group mt-3">
+          <label className="mr-2">Select a file to upload: </label>
+          <input
+            name="file"
+            type="file"
+            id="file"
+            onChange={handleFileChange()}
+          />
+        </div>
+        <hr />
+        {formStatus ? (
+          <div className="text-success mb-2">Your message has been sent.</div>
+        ) : (
+          ""
+        )}
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+export default Form;
