7d5f6bf8200463fd226a9096c7de756bde57e876
Josh Gamache <joshua@button.is>
2023-05-05 13:26:43 -0600

    fix: remove vistigial console.logs

--- app/app/[lng]/analyst/anonymized/[id]/page.tsx
+++ app/app/[lng]/analyst/anonymized/[id]/page.tsx
@@ -4,8 +4,6 @@ export default function Page({ params }) {
   // 👇️ graphQL query endpoint for this role
   const endpoint = "api/analyst/graphql";
 
-  console.log("params");
-  console.log(params);
   return (
     <>
       {/* @ts-expect-error Server Component */}
