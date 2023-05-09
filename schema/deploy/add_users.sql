bbd6bce73e164df088e20afbefcbdc3f51912090
YaokunLin <yaokun1@ualberta.ca>
2023-05-09 16:23:15 -0700

    added 2 email logins

--- schema/deploy/add_users.sql
+++ schema/deploy/add_users.sql
@@ -11,5 +11,7 @@ SELECT eed_private.insert_user('gabriel@button.is', 'analyst');
 SELECT eed_private.insert_user('patrick@button.is', 'analyst');
 SELECT eed_private.insert_user('lin.yaokun1@gmail.com', 'manager');
 SELECT eed_private.insert_user('mike@button.is', 'analyst');
+SELECT eed_private.insert_user('climatetrax.analyst@gmail.com', 'analyst');
+SELECT eed_private.insert_user('climatetrax.manager@gmail.com', 'manager');
 
 COMMIT;
